4dd8b7326a475f3bb1bc1bca5d79baf75185b2cc
vanes430 <vanessimbolon2020@gmail.com>
2025-05-21 11:18:34 +0000

    updetcase

--- plugins/case.js
+++ plugins/case.js
@@ -2,7 +2,7 @@ export async function before(m, { plugins, scrapers, Func }) {
 const prefixCase = [".", "!"];
 
 if (prefixCase.some(prefix => m.body.startsWith(prefix))) {
-const command = m.body.slice(1).toLowerCase(); // Mengambil perintah setelah prefix
+const command = m.body.slice(1).toLowerCase();
 
 switch (command) {
 case "helo":
